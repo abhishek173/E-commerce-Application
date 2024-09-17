5005396514322a2a05cf748d14aa1b582ff39f0e
abhishek173 <abhishekkumar801104@gmail.com>
2024-09-18 02:02:38 +0530

    added Category and Customer table

---
+++ Ecommerce/store/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from  store.category import Category
+from store.customer import Customer
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
